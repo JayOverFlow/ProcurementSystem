b61e7f79f0bbb92b5ec275c1f7af4480d33dc0b1
John Rex Duran <jayoverflow29@gmail.com>
2025-07-14 19:37:15 +0800

    Made some subtle frontend changes to planning and dd. New db schema

---
+++ step_system/app/Controllers/Planning/PlanningDashboardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers\Planning;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
+use App\Models\UserRoleDepartmentModel;
+
+class PlanningDashboardController extends BaseController
+{
+
+    protected $userModel;
+    protected $userRoleDepartmentModel;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+        $this->userRoleDepartmentModel = new UserRoleDepartmentModel();
+    }   
+
+    public function index()
+    {
+        // Get user data via user session using custom helper
+        $userData = $this->loadUserSession();
+
+        // Get dashboard data
+        $dashboardData = [
+            'procurement_status' => null,
+            'faculty_count' => $this->userModel->getAllFacultyCount(),
+            'staff_count' => $this->userModel->getAllStaffCount(),  
+            'subordinates' => null  
+        ];
+
+        // Store data
+        $data = [
+            'user_data' => $userData,
+            'dashboard_data' => $dashboardData
+        ];
+
+        // Return view with stored data
+        return view('user-pages/planning/plan-dashboard', $data);
+    }
+}
